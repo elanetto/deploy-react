0d1cf58691f26d5e16fbeb35f19351f58bd1768d
elanetto <101054274+elanetto@users.noreply.github.com>
2025-02-27 09:56:54 +0100

    Added Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <h1>Hello</h1>
+      <Link to={"/test"}>To test page</Link>
+      <br />
+      <a href="/test">A Href to Test</a>
     
     </>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/test",
+    element: <h1>Test</h1>
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
